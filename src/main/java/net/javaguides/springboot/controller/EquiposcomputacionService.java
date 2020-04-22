f9f4f43fa6b8623b83ac1f0fd486db3e5d5fbd02
unknown <sis004arielpacaja@gmail.com>
2020-04-22 19:54:29 -0400

    servicios de mobiliarioenseres y equiposcomputacion

--- src/main/java/net/javaguides/springboot/controller/EquiposcomputacionService.java
+++ src/main/java/net/javaguides/springboot/controller/EquiposcomputacionService.java
@@ -1,12 +1,22 @@
 package net.javaguides.springboot.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Equiposcomputacion;
 import net.javaguides.springboot.repository.EquiposcomputacionRepository;
 
@@ -20,6 +30,43 @@ public class EquiposcomputacionService {
 	@GetMapping("equiposcomputacion")
 	public List<Equiposcomputacion> getAllEquiposcomputacions(){
 		return this.equiposcomputacionRepository.findAll();
+	}
+	@GetMapping("equiposcomputacion/{id}")
+	public ResponseEntity<Equiposcomputacion> getEquiposcomputacionById(@PathVariable(value = "id") Long equiposcomputacionId)
+			throws ResourceNotFoundException {
+		Equiposcomputacion equiposcomputacion  = equiposcomputacionRepository.findById(equiposcomputacionId)
+				.orElseThrow(() -> new ResourceNotFoundException("Activo no existe para este id :: " + equiposcomputacionId));
+		return ResponseEntity.ok().body(equiposcomputacion);
+	}
+	@PostMapping("equiposcomputacion")
+	public Equiposcomputacion createEquiposcomputacion(Equiposcomputacion equiposcomputacion) {
+		return equiposcomputacionRepository.save(equiposcomputacion);
+	}
+	@PutMapping("equiposcomputacion/{id}")
+	public ResponseEntity<Equiposcomputacion> updateEquiposcomputacion(@PathVariable(value = "id") Long equiposcomputacionId,
+			@Valid Equiposcomputacion equiposcomputacionDetails) throws ResourceNotFoundException {
+		Equiposcomputacion equiposcomputacion = equiposcomputacionRepository.findById(equiposcomputacionId)
+				.orElseThrow(() -> new ResourceNotFoundException("Adquisicion no exite para este id :: " + equiposcomputacionId));
+		
+		equiposcomputacion.setMarca(equiposcomputacionDetails.getMarca());
+		equiposcomputacion.setNro_serial(equiposcomputacionDetails.getNro_serial());
+		equiposcomputacion.setColor(equiposcomputacionDetails.getColor());
+		equiposcomputacion.setTipo(equiposcomputacionDetails.getTipo());
+		equiposcomputacion.setIndustria(equiposcomputacionDetails.getIndustria());
+
+		return ResponseEntity.ok(this.equiposcomputacionRepository.save(equiposcomputacion));
+	}
+	@DeleteMapping("equiposcomputacion/{id}")
+	public Map<String, Boolean> deleteequiposcomputacion(@PathVariable(value = "id") Long equiposcomputacionId)
+			throws ResourceNotFoundException {
+		Equiposcomputacion equiposcomputacion = equiposcomputacionRepository.findById(equiposcomputacionId)
+				.orElseThrow(() -> new ResourceNotFoundException("Adquisicion no existe para este id :: " + equiposcomputacionId));
+
+		this.equiposcomputacionRepository.delete(equiposcomputacion);
+		
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return response;
 	}
 
 }
--- src/main/java/net/javaguides/springboot/controller/MobiliarioenseresService.java
+++ src/main/java/net/javaguides/springboot/controller/MobiliarioenseresService.java
@@ -1,12 +1,22 @@
 package net.javaguides.springboot.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Mobiliarioenseres;
 import net.javaguides.springboot.repository.MobiliarioenseresRepository;
 
@@ -20,6 +30,45 @@ public class MobiliarioenseresService {
 	@GetMapping("mobiliarioenseres")
 	public List<Mobiliarioenseres> getAllMobiliarioenseres(){
 		return this.mobiliarioenseresRepository.findAll();
+	}
+	
+	@GetMapping("mobiliarioenseres/{id}")
+	public ResponseEntity<Mobiliarioenseres> getMobiliarioenseresById(@PathVariable(value = "id") Long mobiliarioenseresId)
+			throws ResourceNotFoundException {
+		Mobiliarioenseres mobiliarioenseres = mobiliarioenseresRepository.findById(mobiliarioenseresId)
+				.orElseThrow(() -> new ResourceNotFoundException("Seccion no encontrada para esta identificacion : : " + mobiliarioenseresId));
+		return ResponseEntity.ok().body(mobiliarioenseres);
+	}
+	
+	@PostMapping("mobiliarioenseres")
+	public Mobiliarioenseres createMobiliarioenseres(Mobiliarioenseres mobiliarioenseres) {
+		return mobiliarioenseresRepository.save(mobiliarioenseres);
+	}
+
+	@PutMapping("mobiliarioenseres/{id}")
+	public ResponseEntity<Mobiliarioenseres> updateMobiliarioenseres(@PathVariable(value = "id") Long mobiliarioenseresId,
+			@Valid Mobiliarioenseres mobiliarioenseresDetails) throws ResourceNotFoundException {
+		Mobiliarioenseres mobiliarioenseres = mobiliarioenseresRepository.findById(mobiliarioenseresId)
+				.orElseThrow(() -> new ResourceNotFoundException("Seccion no encontrada para esta identificacion : : " + mobiliarioenseresId));
+
+		
+		mobiliarioenseres.setColor(mobiliarioenseresDetails.getColor());
+		mobiliarioenseres.setId_activo(mobiliarioenseresDetails.getId_activo());
+		mobiliarioenseres.setMaterial(mobiliarioenseresDetails.getMaterial());
+		return ResponseEntity.ok(this.mobiliarioenseresRepository.save(mobiliarioenseres));
+	}
+
+	@DeleteMapping("mobiliarioenseres/{id}")
+	public Map<String, Boolean> deleteMobiliarioenseres(@PathVariable(value = "id") Long mobiliarioenseresId)
+			throws ResourceNotFoundException {
+		Mobiliarioenseres mobiliarioenseres = mobiliarioenseresRepository.findById(mobiliarioenseresId)
+				.orElseThrow(() -> new ResourceNotFoundException("Seccion no encontrada para esta identificacion : : " + mobiliarioenseresId));
+
+		this.mobiliarioenseresRepository.delete(mobiliarioenseres);
+		
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return response;
 	}
 
 }
