1ff725fe0d8ceb88f5420edcb50eece8812d6366
FernandoLFMC <ferchitolfmc@gmail.com>
2020-12-28 01:15:30 -0400

    actualizacion de igualdad

--- src/main/java/net/javaguides/springboot/TacficrudApplication.java
+++ src/main/java/net/javaguides/springboot/TacficrudApplication.java
@@ -2,12 +2,21 @@ package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.session.RedisSessionProperties.ConfigureAction;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TacficrudApplication {
+public class TacficrudApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TacficrudApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		 
+		return builder.sources(TacficrudApplication.class);
 	}
 
 }
--- src/main/java/net/javaguides/springboot/security/WebSecurityConfig.java
+++ src/main/java/net/javaguides/springboot/security/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/api/auth/**"/*,"/api/v1/**"*/).permitAll()
+                .antMatchers("/api/auth/**","/api/v1/uploads/img/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
